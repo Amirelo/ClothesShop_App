837cf6f953bc2ab6335a09938100e03f9d8111ab
Amirelo <80700398+Amirelo@users.noreply.github.com>
2024-01-03 21:38:28 +0700

    Add navigation from sign_in_screen to home_screen

--- lib/src/screens/auth/sign_in_screen.dart
+++ lib/src/screens/auth/sign_in_screen.dart
@@ -1,4 +1,5 @@
 import 'package:clothes_shop/src/colors/theme_default.dart';
+import 'package:clothes_shop/src/screens/main/home_screen.dart';
 import 'package:clothes_shop/src/widgets/custom_button.dart';
 import 'package:clothes_shop/src/widgets/custom_input.dart';
 import 'package:clothes_shop/src/widgets/custom_text.dart';
@@ -10,88 +11,96 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const CustomImage(
-          src: 'assets/images/logo_white.png',
-          width: 72,
-          height: 72,
-          marginTop: 100,
-          marginBottom: 16,
-          borderRadius: 16,
-          type: "asset",
-        ),
-        CustomText(
-          text: "Welcome to Lafyuu",
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-          textColor: ThemeDefault().textColor,
-        ),
-        CustomText(
-          text: "Sign in to continue",
-          fontSize: 12,
-          textColor: ThemeDefault().secondaryTextColor,
-          marginBottom: 28,
-        ),
-        const CustomInput(
-          placeholder: "Email",
-          icon: Icons.email,
-        ),
-        const CustomInput(
-          placeholder: "Password",
-          icon: Icons.lock,
-          marginBottom: 12,
-        ),
-        CustomButton(
-          onPressed: _buttonAction,
-          text: "Sign in",
-          backgroundColor: ThemeDefault().primaryColor,
-        ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 16),
-          child: Row(
+    void onSignInPressed() {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+    }
+
+    return Scaffold(
+      body: Column(
+        children: [
+          const CustomImage(
+            src: 'assets/images/logo_white.png',
+            width: 72,
+            height: 72,
+            marginTop: 100,
+            marginBottom: 16,
+            borderRadius: 16,
+            type: "asset",
+          ),
+          CustomText(
+            text: "Welcome to Lafyuu",
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            textColor: ThemeDefault().textColor,
+          ),
+          CustomText(
+            text: "Sign in to continue",
+            fontSize: 12,
+            textColor: ThemeDefault().secondaryTextColor,
+            marginBottom: 28,
+          ),
+          const CustomInput(
+            placeholder: "Email",
+            icon: Icons.email,
+          ),
+          const CustomInput(
+            placeholder: "Password",
+            icon: Icons.lock,
+            marginBottom: 12,
+          ),
+          CustomButton(
+            onPressed: onSignInPressed,
+            text: "Sign in",
+            backgroundColor: ThemeDefault().primaryColor,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Expanded(
+                  child: Divider(),
+                ),
+                CustomText(
+                  text: "OR",
+                  textColor: ThemeDefault().secondaryTextColor,
+                  fontWeight: FontWeight.bold,
+                  marginBottom: 0,
+                  marginLeft: 16,
+                  marginRight: 16,
+                ),
+                const Expanded(child: Divider()),
+              ],
+            ),
+          ),
+          const CustomButton(
+              onPressed: _buttonAction, text: "Login with Google"),
+          const CustomButton(
+            onPressed: _buttonAction,
+            text: "Login with Facebook",
+            marginBottom: 16,
+          ),
+          CustomText(
+            text: "Forgot password?",
+            textColor: ThemeDefault().primaryColor,
+            fontWeight: FontWeight.bold,
+            fontSize: 12,
+          ),
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Expanded(
-                child: Divider(),
-              ),
+              const CustomText(text: "Don't have an account? "),
               CustomText(
-                text: "OR",
-                textColor: ThemeDefault().secondaryTextColor,
+                text: 'Register',
+                textColor: ThemeDefault().primaryColor,
                 fontWeight: FontWeight.bold,
-                marginBottom: 0,
-                marginLeft: 16,
-                marginRight: 16,
-              ),
-              const Expanded(child: Divider()),
+                fontSize: 12,
+              )
             ],
           ),
-        ),
-        const CustomButton(onPressed: _buttonAction, text: "Login with Google"),
-        const CustomButton(
-          onPressed: _buttonAction,
-          text: "Login with Facebook",
-          marginBottom: 16,
-        ),
-        CustomText(
-          text: "Forgot password?",
-          textColor: ThemeDefault().primaryColor,
-          fontWeight: FontWeight.bold,
-          fontSize: 12,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const CustomText(text: "Don't have an account? "),
-            CustomText(
-              text: 'Register',
-              textColor: ThemeDefault().primaryColor,
-              fontWeight: FontWeight.bold,
-              fontSize: 12,
-            )
-          ],
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/src/screens/main/home_screen.dart
+++ lib/src/screens/main/home_screen.dart
@@ -5,30 +5,32 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return (Column(
-      children: [
-        const TextField(decoration: InputDecoration(labelText: "Search")),
-        const Icon(Icons.search),
-        const Icon(Icons.favorite),
-        const Icon(Icons.notifications),
-        Image.asset(
-          'assets/images/logo_blue.png',
-          width: 343208,
-          height: 100,
-        ),
-        const Icon(Icons.fiber_manual_record),
-        const Text("Category"),
-        const Text("More Category"),
-        const Text("Flash Sale"),
-        const Text("See More"),
-        const Text("Mega Sale"),
-        const Text("See More"),
-        Image.asset(
-          'assets/images/logo_blue.png',
-          width: 343208,
-          height: 100,
-        ),
-      ],
-    ));
+    return Scaffold(
+      body: (Column(
+        children: [
+          const TextField(decoration: InputDecoration(labelText: "Search")),
+          const Icon(Icons.search),
+          const Icon(Icons.favorite),
+          const Icon(Icons.notifications),
+          Image.asset(
+            'assets/images/logo_blue.png',
+            width: 343208,
+            height: 100,
+          ),
+          const Icon(Icons.fiber_manual_record),
+          const Text("Category"),
+          const Text("More Category"),
+          const Text("Flash Sale"),
+          const Text("See More"),
+          const Text("Mega Sale"),
+          const Text("See More"),
+          Image.asset(
+            'assets/images/logo_blue.png',
+            width: 343208,
+            height: 100,
+          ),
+        ],
+      )),
+    );
   }
 }
